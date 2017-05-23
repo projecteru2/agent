699a82d507dd1e769acfde829680af31dd5c747d
Zhang Ye <zhangye@ricebook.com>
2017-05-23 12:59:45 +0800

    update: 提高日志时间戳精度

--- common/common.go
+++ common/common.go
@@ -2,14 +2,14 @@ package common
 
 const (
 	DEFAULT_ETCD_PREFIX = "eru"
-	ERU_AGENT_VERSION   = "2.0.10a"
+	ERU_AGENT_VERSION   = "2.0.10b"
 	DOCKER_CLI_VERSION  = "v1.23"
 
 	STATUS_DIE     = "die"
 	STATUS_START   = "start"
 	STATUS_DESTROY = "destroy"
 
-	DATETIME_FORMAT = "2006-01-02 15:04:05"
+	DATETIME_FORMAT = "2006-01-02 15:04:05.999999"
 	CNAME_NUM       = 3
 
 	CGROUP_BASE_PATH = "/sys/fs/cgroup/%s/docker/%s/%s"
