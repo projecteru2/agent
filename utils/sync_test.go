6a85adcc6e0ed53ee1aadfa9048b92927243dadc
anrs <anders.hu@shopee.com>
2021-02-19 18:20:29 +0800

    fix: data race while multiple invoking of checkOneContainer

---
+++ utils/sync_test.go
@@ -0,0 +1,61 @@
+package utils
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCAS(t *testing.T) {
+	key := "key"
+
+	var cas GroupCAS
+	free, acquired := cas.Acquire(key)
+	require.True(t, acquired)
+	require.NotNil(t, free)
+
+	free1, acquired1 := cas.Acquire(key)
+	require.False(t, acquired1)
+	require.Nil(t, free1)
+
+	free()
+	free1, acquired1 = cas.Acquire(key)
+	require.True(t, acquired1)
+	require.NotNil(t, free1)
+}
+
+func TestCASConccurently(t *testing.T) {
+	var cas GroupCAS
+	var wg sync.WaitGroup
+
+	n := 1000
+	key := "key"
+	var sum int32
+	wg.Add(n)
+
+	for i := 0; i < n; i++ {
+		go func(idx int) {
+			defer wg.Done()
+
+			_, acq := cas.Acquire(fmt.Sprintf("%d", idx))
+			require.True(t, acq)
+
+			free, acquired := cas.Acquire(key)
+			if !acquired {
+				return
+			}
+
+			// makes sure that there're only one thread has been acquired.
+			require.True(t, atomic.CompareAndSwapInt32(&sum, 0, 1), atomic.LoadInt32(&sum))
+			// marks there's no thread is acquired in advance.
+			require.True(t, atomic.CompareAndSwapInt32(&sum, 1, 0))
+
+			free()
+		}(i)
+	}
+
+	wg.Wait()
+}
